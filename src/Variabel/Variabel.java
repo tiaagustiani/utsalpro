30416858334d2cf2d1b3b1440a89bdb6fc5503f4
tiaagustiani <tia.agustiani@unida.ac.id>
2021-11-20 10:01:32 +0700

    Input jawaban
    Jawaban

---
+++ src/Variabel/Variabel.java
@@ -0,0 +1,14 @@
+package Variabel;
+
+public class Variabel {
+	public static void main(String[] args) {
+		String salam = "Assalamu'alaikum";
+		String nama = "Tia Agustiani";
+		int tahun_lahir = 2000;
+		
+		System.out.println(salam);
+		System.out.println("Nama Saya : " + nama);
+		System.out.println("Saya Lahir di Tahun : " + tahun_lahir);
+
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module utsalpro {
+}
